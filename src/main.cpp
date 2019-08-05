fb0cfc1759644b73890de2d65114e3ae3cf82b6e
Jyry Hjelt <hjelt.jyry@gmail.com>
2019-08-05 17:25:34 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// main.cpp
+// Written by: Jyry Hjelt
+//
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
---
+++ tests/testmain.cpp
@@ -0,0 +1,11 @@
+// 
+// testmain.cpp
+// Test cases for the program
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("Example test case", "[catch2]"){
+	REQUIRE(1==1);
+}
